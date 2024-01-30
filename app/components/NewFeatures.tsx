0ef3c248d86679d719c42712af97bedc6ce09e1e
victorlouly <eduardodiniz.contato@gmail.com>
2024-01-30 20:22:00 -0300

    new

--- app/components/NewFeatures.tsx
+++ app/components/NewFeatures.tsx
@@ -18,7 +18,7 @@ const NewFeatures = ({ imgUrl, title, subtitle }: NewFeaturesProps) => (
       />
     </div>
     <h1 className="mt-[26px] font-bold text-[24px] leading-[30px] text-white">
-      &nbsp; Title {title}&nbsp;
+      &nbsp; {title}&nbsp;
     </h1>
     <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]">
       &nbsp;
