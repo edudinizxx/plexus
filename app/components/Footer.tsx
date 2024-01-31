f963284def76fe68cc10b6f8aec7a67bf5fc9fd8
victorlouly <eduardodiniz.contato@gmail.com>
2024-01-31 18:08:28 -0300

    fet

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -17,9 +17,12 @@ const Footer = () => (
     <div className="innerWidth mx-auto flex flex-col gap-8">
       <div className="flex flex-col">
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-        <div className=" flex justify-between flex-wrap gap-8">
+        <div className=" flex justify-between items-center flex-wrap gap-8">
          <img src="/logo.svg" alt="" />
-          <p className="font-normal text-[14px] text-white opacity-50">
+         <p className="font-normal text-[14px] text-white opacity-50">
+          Politicas de Privacidade
+          </p>
+          <p className="font-normal text-[14px]  text-white opacity-50">
           2020 - 2024 Agência Plexus. All rights reserved.
           </p>
         </div>
