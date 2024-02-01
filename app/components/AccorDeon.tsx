f3c16085c2a23873561504730c601d2e2e039d4e
victorlouly <91222566+victorlouly@users.noreply.github.com>
2024-02-01 17:32:43 -0300

    fex

--- app/components/AccorDeon.tsx
+++ app/components/AccorDeon.tsx
@@ -5,8 +5,18 @@ import {Accordion, AccordionItem} from "@nextui-org/react";
 import { TitleText, TypingText } from "../components";
 
 const AccorDeon = () => {
-  const defaultContent =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const um =
+  "Você pode ver nosso portfólio de design através do link";
+  const dois =
+  "O tempo necessário para desenvolver o projeto pode variar significativamente com base em diversos fatores, como o tipo de projeto que você precisa, complexidade, funcionalidades desejadas dentre outros fatores.";
+  const tres =
+  "Mantemos todos os nossos processos claros e transparentes para cada membro da equipe e para o cliente. Você obtém acesso ao controle de tempo, onde pode ver os marcos, sprints semanais e o prazo. Se desejar, criamos um grupo no WhatsApp.";
+  const quatro =
+  "Nosso processo de design anda de mãos dadas com o desenvolvimento para evitar qualidade indesejável após o lançamento do projeto. Construímos relacionamentos de longo prazo com nossos clientes e oferecemos suporte após o lançamento do projeto.";
+  const cinco =
+  "Iniciar um projeto conosco é simples! Entre em contato através do WhatsApp. ";
+  const seis =
+  "Utilizamos uma abordagem personalizada, analisando a identidade única de sua marca para criar designs exclusivos e impactantes.";
 
 return (
  <div className="container mx-auto mt-14 mt-36">
@@ -23,27 +33,28 @@ return (
     <div className="flex justify-center  mb-20">  
     <div className="text-white font-light">
     <Accordion>
-      <AccordionItem key="1" aria-label="Accordion 1" title="Quanto tempo leva para desenvolver um website?">
-        {defaultContent}
+      <AccordionItem key="1" aria-label="Accordion 1" title="Onde posso ver seu portfólio de design?">
+        {um}
       </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Quais são os custos envolvidos no desenvolvimento de um website?">
-        {defaultContent}
+      <AccordionItem key="2" aria-label="Accordion 2" title="Com que rapidez você pode começar a trabalhar no meu projeto?">
+        {dois}
       </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Vocês ofoerem serviços de otimização para mecanismos de busca?">
-        {defaultContent}
+      <AccordionItem key="3" aria-label="Accordion 3" title="Como você mantém a comunicação durante o projeto?">
+        {tres}
       </AccordionItem>
     </Accordion>
     </div>
     <div className="text-white font-light">
     <Accordion>
-      <AccordionItem key="1" aria-label="Accordion 1" title="Como funciona o processo de design e desenvolvimento do website?">
-        {defaultContent}
+      <AccordionItem key="1" aria-label="Accordion 1" title="Você fornece serviços pós-lançamento?">
+        {quatro}
       </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Meu website será responsivo em dispositivos móveis?">
-        {defaultContent}
+      <AccordionItem key="2" aria-label="Accordion 2" title="Como posso iniciar um projeto com vocês e qual é o processo envolvido?">
+        {cinco}
       </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Como posso realizar atualizações no meu website após o lançamento?">
-        {defaultContent}
+      <AccordionItem key="3" aria-label="Accordion 3" title="Como podemos garantir que o design criado para minha marca seja exclusivo e impactante?
+">
+        {seis}
       </AccordionItem>
     </Accordion>
     </div>
